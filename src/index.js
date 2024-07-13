abed473439a29589bd79de8ed93c1d297077220a
Ange_Tech(Angel_Molina) <166965796+AngelTech90@users.noreply.github.com>
2024-07-12 23:42:15 -0600

    Improving main slider component, tailwind implementation, initializing the steps slider, and adding index.js for fix little details in alpine components, depuring the main section

---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+/* *This file is for make css corrections in our page components */
+
+document.getElementById('webAppImage').style.marginTop = '9.5%';
+
+document.getElementById('phoneAppImage').style.marginTop = '-10%';
+
+document.getElementById('phoneAppImage').style.marginRight = '34%';
+
+document.getElementById('phoneAppImage').style.marginLeft = '16%';
+
+
+document.getElementById('logoContainer').onmouseover = () => {
+
+    document.getElementById('pageLogoSpan').style.color = '#079cff';
+
+}
+
+document.getElementById('logoContainer').onmouseout = () => {
+
+    document.getElementById('pageLogoSpan').style.color = '#ffffff';
+
+}
+
