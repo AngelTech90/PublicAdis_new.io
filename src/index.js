201c875bd372418dab90d3982dabcbc099de9175
Ange_Tech(Angel_Molina) <166965796+AngelTech90@users.noreply.github.com>
2024-07-13 11:54:13 -0600

    Commit 2 for upload changes to page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
 /* *This file is for make css corrections in our page components */
 
+//* *This is for adjust the slider images
 document.getElementById('webAppImage').style.marginTop = '9.5%';
 
 document.getElementById('phoneAppImage').style.marginTop = '-10%';
@@ -10,6 +11,7 @@ document.getElementById('phoneAppImage').style.marginRight = '34%';
 document.getElementById('phoneAppImage').style.marginLeft = '16%';
 
 
+//* *This is for adjust the page logo hover: 
 document.getElementById('logoContainer').onmouseover = () => {
 
     document.getElementById('pageLogoSpan').style.color = '#079cff';
